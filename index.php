22d312817e5b44f7bdc5336056117cdc8491135d
IdentalerMaxima <koppany.naray@gmail.com>
2023-01-13 18:39:06 +0100

    Centered footer img

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 <head>
     <title>Leguan Tattoo</title>
+
     <link rel="stylesheet" href="res\style.css">
 
     <script src="https://kit.fontawesome.com/cd030ff4ce.js" crossorigin="anonymous"></script>
@@ -10,7 +11,6 @@
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-
     <link href="https://fonts.googleapis.com/css2?family=Spectral:ital,wght@0,200;0,300;1,300&display=swap"
         rel="stylesheet">
 
@@ -26,20 +26,14 @@
         href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300&family=Spectral:ital,wght@1,200&display=swap"
         rel="stylesheet">
 
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Aboreto&family=Girassol&family=IM+Fell+French+Canon+SC&family=Jim+Nightshade&display=swap');
-    </style>
-
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300&family=Spectral:ital,wght@1,200&display=swap');
-    </style>
-
     <script src="https://kit.fontawesome.com/51d8255158.js" crossorigin="anonymous"></script>
 
     <script src="https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll/dist/smooth-scroll.polyfills.min.js"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
+
     <script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
 
 </head>
